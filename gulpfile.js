aeddff0179247aef389a34988987d3a0a9a71412
Hongbusi <q951686662@qq.com>
2021-03-27 17:06:25 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const { src, dest } = require('gulp');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 exports.default = function() {
+  let plugins = [
+    autoprefixer({ overrideBrowserslist: ['last 2 versions'] }),
+    cssnano()
+  ];
   return src('styles/*.css')
+    .pipe(concat('index.min.css'))
+    .pipe(postcss(plugins))
     .pipe(dest('dist/'));
 }
