1586d03f785a41cc195350e5f8f26f698d12ab9f
Hongbusi <q951686662@qq.com>
2021-04-21 20:00:15 +0800

    less

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcssImport = require('postcss-import');
+const less = require('postcss-less');
 
 module.exports = {
   plugins: [
