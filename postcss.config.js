db07db039db3b540cfd080650e900ab333e79df5
hongyongchun <hongyongchun@edusoho.com>
2021-05-06 17:55:50 +0800

    less

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const postcssImport = require('postcss-import');
-const less = require('postcss-less');
 
 module.exports = {
   plugins: [
     autoprefixer(),
-    postcssImport(),
     cssnano()
-  ],
-  // syntax: 'postcss-less'
+  ]
 }
